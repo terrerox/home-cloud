922b52399ae520d0533fbef855b109296825c75b
Francis <terrero0611@gmail.com>
2020-09-06 00:38:26 -0400

    frontend configured

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import axios from "axios";
+import router from './router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$http = axios;
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
